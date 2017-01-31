e4feeb3cfcbd6a79ba6746fbaa27df66da7cccd6
Kacper <kacdrapala@gmail.com>
2017-02-01 00:03:39 +0100

    Signed-off-by: Kacper <kacdrapala@gmail.com>

---
+++ CoffeeMakeTest/DrinkTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoffeeMakeTest
+{
+    [TestClass]
+    public class DrinkTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
